c583b09887260952fb77cedb8af257037a869423
joigs <jose.saez2001@alumnos.ubiobio.cl>
2025-10-22 16:49:32 -0300

    configuracion del sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 # config/initializers/sidekiq.rb
+redis_url = ENV.fetch("SIDEKIQ_REDIS_URL", "redis://localhost:6379/1")
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0"),
-                   namespace: "sidekiq_registro_#{Rails.env}" }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0"),
-                   namespace: "sidekiq_registro_#{Rails.env}" }
+  config.redis = { url: redis_url }
 end
