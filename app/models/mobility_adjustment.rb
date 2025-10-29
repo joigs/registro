7457e3e5de480003ce112ece8f0de756c3851451
joigs <jose.saez2001@alumnos.ubiobio.cl>
2025-10-29 16:14:41 -0300

    log

--- app/models/mobility_adjustment.rb
+++ app/models/mobility_adjustment.rb
@@ -5,3 +5,27 @@ class MobilityAdjustment < ApplicationRecord
   scope :in_month, ->(y,m){ where(fecha: Date.new(y,m,1)..Date.civil(y,m,-1)) }
   scope :in_year,  ->(y,upto_m=12){ where(fecha: Date.new(y,1,1)..Date.civil(y,upto_m,-1)) }
 end
+
+
+
+
+
+
+################## LOGS / EJEMPLOS ######################
+#   MobilityAdjustment.create!(
+#     empresa:         "Arauco",
+#     mandante_rut:    "85805200",
+#     mandante_nombre: "Forestal Arauco SA",
+#     uf:              107.3,
+#     servicios:       67,
+#     fecha:           Date.new(2025, 10, 1)
+#   )
+#
+#   MobilityAdjustment.create!(
+#     empresa:         "Transporte de personal CMPC",
+#     mandante_rut:    nil,
+#     mandante_nombre: "Forestal Mininco SA",
+#     uf:              34.2,
+#     servicios:       26,
+#     fecha:           Date.new(2025, 10, 1)
+#   )
