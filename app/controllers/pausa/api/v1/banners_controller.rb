d696fbc0742a5891b64f6222ff71985e1c47321d
joigs <jose.saez2001@alumnos.ubiobio.cl>
2025-11-02 02:56:14 -0300

    prueba

--- app/controllers/pausa/api/v1/banners_controller.rb
+++ app/controllers/pausa/api/v1/banners_controller.rb
@@ -1,15 +1,18 @@
+# app/controllers/pausa/api/v1/banners_controller.rb
 # frozen_string_literal: true
 module Pausa
   module Api
     module V1
       class BannersController < ApplicationController
-        before_action :authenticate!
         skip_before_action :verify_authenticity_token
         skip_before_action :protect_pages
-        def index
-          is_admin = current_user&.admin? || false
-          scoped   = Pausa::AppBanner.enabled.for_admin(is_admin)
+        before_action :authenticate!
 
+        def index
+          # 👇 usar la variable de instancia seteada por authenticate!
+          is_admin = @current_user&.admin? || false
+
+          scoped = Pausa::AppBanner.enabled.for_admin(is_admin)
           inline = scoped.for_kind("inline").order(created_at: :desc).limit(1).first
           modal  = scoped.for_kind("modal").order(created_at: :desc).limit(1).first
 
