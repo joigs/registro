9b4546f49caab90065a7be66830e6a85a25dc34a
joigs <jose.saez2001@alumnos.ubiobio.cl>
2026-03-10 18:15:00 -0300

    v2

--- app/channels/camioneta/checkeo_channel.rb
+++ app/channels/camioneta/checkeo_channel.rb
@@ -1,10 +1,25 @@
 module Camioneta
   class CheckeoChannel < ApplicationCable::Channel
+    BOOLEAN_FIELDS = %w[
+      botiquin gata cadenas llave_rueda antena_radio permiso_circulacion
+      revision_tecnica soap alcohol protector_solar carpeta panos_limpieza
+      conos radio_comunicacion espejo_inspeccion toldo pie_de_metro tintas arnes
+    ].freeze
+
+    INTEGER_FIELDS = %w[
+      extintor kit_derrame
+      falta_diclofenaco_cant falta_guantes_cant falta_parche_curita_cant
+      falta_gasa_cant falta_venda_cant falta_suero_cant
+      falta_tela_adhesiva_cant falta_palitos_cant
+    ].freeze
+
     def subscribed
-      @checkeo = Camioneta::CheckCheckeo.includes(:check_checkeo_usuarios).find_by(id: params[:checkeo_id])
+      @checkeo = Camioneta::CheckCheckeo
+                   .includes(:check_checkeo_usuarios)
+                   .find_by(id: params[:checkeo_id])
 
       reject and return unless @checkeo
-      reject and return unless autorizado?(@checkeo)
+      reject and return unless autorizado?
 
       stream_for @checkeo
     end
@@ -12,52 +27,54 @@ module Camioneta
     def unsubscribed
     end
 
-
+    # subscription.perform("update_fields", { changes: {...}, client_id: "abc123" })
     def update_fields(data)
       return unless @checkeo
-      return unless autorizado?(@checkeo)
+      return unless autorizado?
 
-      cambios = filtrar_cambios(data["changes"] || {})
-      return if cambios.empty?
+      incoming = data["changes"].is_a?(Hash) ? data["changes"] : {}
+      changes = sanitize_changes(incoming)
+      return if changes.empty?
 
-      if @checkeo.update(cambios)
-        payload = {
-          type: "checkeo_updated",
-          checkeo_id: @checkeo.id,
-          changes: @checkeo.saved_changes.slice(*cambios.keys.map(&:to_s)),
-          updated_at: @checkeo.updated_at,
-          updated_by_id: current_usuario.id,
-          client_id: data["client_id"]
-        }
-
-        Camioneta::CheckeoChannel.broadcast_to(@checkeo, payload)
+      if @checkeo.update(changes)
+        Camioneta::CheckeoChannel.broadcast_to(
+          @checkeo,
+          {
+            type: "checkeo_updated",
+            checkeo_id: @checkeo.id,
+            changes: changes,
+            updated_at: @checkeo.updated_at.iso8601(3),
+            updated_by_id: current_usuario.id,
+            client_id: data["client_id"]
+          }
+        )
       else
-        transmit({
-                   type: "error",
-                   errors: @checkeo.errors.full_messages
-                 })
+        transmit(
+          type: "error",
+          errors: @checkeo.errors.full_messages
+        )
       end
     end
 
     private
 
-    def autorizado?(checkeo)
-      checkeo.check_checkeo_usuarios.exists?(check_usuario_id: current_usuario.id)
+    def autorizado?
+      @checkeo.check_checkeo_usuarios.exists?(check_usuario_id: current_usuario.id)
     end
 
-    def filtrar_cambios(changes)
-      permitted_keys = %w[
-        fecha_chequeo completado corregido_fuera_de_fecha
-        extintor kit_derrame botiquin gata cadenas llave_rueda
-        antena_radio permiso_circulacion revision_tecnica soap alcohol
-        protector_solar carpeta panos_limpieza conos radio_comunicacion
-        espejo_inspeccion toldo pie_de_metro tintas arnes
-        falta_diclofenaco_cant falta_guantes_cant falta_parche_curita_cant
-        falta_gasa_cant falta_venda_cant falta_suero_cant
-        falta_tela_adhesiva_cant falta_palitos_cant
-      ]
+    def sanitize_changes(incoming)
+      allowed = BOOLEAN_FIELDS + INTEGER_FIELDS
 
-      changes.slice(*permitted_keys)
+      incoming.slice(*allowed).each_with_object({}) do |(key, value), acc|
+        acc[key] =
+          if BOOLEAN_FIELDS.include?(key)
+            ActiveModel::Type::Boolean.new.cast(value)
+          elsif value.nil? || value == ""
+            nil
+          else
+            value.to_i
+          end
+      end
     end
   end
 end
